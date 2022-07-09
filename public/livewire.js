0590c4d6e1933b52ed27deb7a4d1a0ff54e8dccf
HlaingMinThan <hlaingminthan@92@gmail.com>
2022-07-09 15:32:11 +0700

    bring dom into life

---
+++ public/livewire.js
@@ -0,0 +1,31 @@
+let snapshotElements = document.querySelectorAll("[wire\\:snapshot]");
+snapshotElements.forEach((el) => {
+    let snapshot = JSON.parse(el.getAttribute("wire:snapshot"));
+    el.__livewire = snapshot;
+    initWireClick(el);
+});
+
+function initWireClick(el) {
+    el.addEventListener("click", (clickEl) => {
+        if (!clickEl.target.hasAttribute("wire:click")) return;
+        //this is the wire:click button handler
+        let callMethod = clickEl.target.getAttribute("wire:click");
+        sendRequest(el, { callMethod });
+    });
+}
+
+async function sendRequest(el, payload) {
+    let snapshot = el.__livewire;
+
+    let res = await fetch("/livewire", {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+        },
+        body: JSON.stringify({ snapshot, ...payload }),
+    });
+    let response = await res.json();
+    let { html, snapshot: newSnapshot } = response;
+    el.__livewire = newSnapshot;
+    el.innerHTML = html;//need to update intelligently
+}
--- routes/web.php
+++ routes/web.php
@@ -15,7 +15,6 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::post('/livewire', function () {
-
     $livewire = new Livewire;
 
     $component = $livewire->fromSnapshot(request('snapshot'));
