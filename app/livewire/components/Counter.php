4c5d110183dd8b580be8d599a56324cd0670262a
HlaingMinThan <hlaingminthan@92@gmail.com>
2022-07-09 13:21:02 +0700

    how click should work template

--- app/livewire/components/Counter.php
+++ app/livewire/components/Counter.php
@@ -7,8 +7,13 @@ class Counter
     public $count = 8;
     public $fee = 'price';
 
+    public function increment()
+    {
+        $this->count = $this->count + 1;
+    }
+
     public function render()
     {
-        return '<h1>{{$count}} {{$fee}}</h1>';
+        return '<h1>{{$count}} {{$fee}}</h1> <button wire:click="increment">increment</button>';
     }
 }
